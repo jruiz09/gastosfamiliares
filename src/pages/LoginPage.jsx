4c9f0ec9de0da9238909868aa5a2bfa310e64fa5
JuanJRuiz <juaruiz@andreani.com>
2026-05-28 11:47:04 -0300

    prueba

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -133,7 +133,7 @@ navigate('/');
               mb-2
             "
           >
-            Gastos Familiares
+            Gastos Familiares 
           </h1>
 
           <p className="text-zinc-400 mb-8">
